fdf79ee36e2ed85304269ee5c006fa4810805b44
htrhtrhrthr <86225249+htrhtrhrthr@users.noreply.github.com>
2026-09-17 03:09:23 +0800

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+export default {
+  async fetch(request, env, ctx) {
+    return new Response("Funbox Beyblade Radar is running!");
+  },
+
+  async scheduled(event, env, ctx) {
+    console.log("Funbox Beyblade Radar scheduled check triggered");
+  }
+};
